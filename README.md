e82c8fb076b81ec7fb02fb5c14c84322289f17e7
jeamxn <admin@chicken-moo.com>
2024-09-23 20:35:41 +0900

    feat: readme & comment

--- README.md
+++ README.md
@@ -1,36 +1,62 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+<p align="center">
+  <img src="./public/logo.svg" height="50px">
+  <p align="center"><b><i>모의고사 키우기 :: 모의고사를 한 곳에서 모아보자!</i></b></p>
+</p>
 
-## Getting Started
+# 1. 모의고사 키우기
 
-First, run the development server:
+[EBSi 기출문제](https://www.ebsi.co.kr/ebs/xip/xipc/previousPaperList.ebs?targetCd=D300)에서 로그인 후 다운로드해야 하는 모의고사 기출문제를 로그인 없이 한 곳에서 모아볼 수 있는 플랫폼입니다.
+
+개발에 참여하고 싶다면 PR을 보내주세요! (Issue도 환영합니다.)
+
+# 2. 설정
+
+이 프로그램은 Next.JS로 작성되었습니다.
+
+## 1) 환경 설정
+
+### ① Node.js
+`node v20.13.1`으로 개발되었습니다.
+> [`nvm`](https://github.com/nvm-sh/nvm) 사용을 권장합니다.
+
+### ② Bun
+[`bun`](https://bun.sh)을 사용합니다. `v1.1.9`
+
+
+# 3. 개발
+
+## 1) 개발 서버 실행
+
+개발을 시작하기 전에 아래의 명령어를 입력하여 개발 서버를 실행해주세요.
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
 bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+개발 서버가 실행되면 [http://localhost:3000](http://localhost:3000)에서 결과를 확인할 수 있습니다.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+`src/` 폴더 안의 소스코드를 수정하여 페이지를 수정할 수 있습니다. 파일을 수정할 때마다 페이지가 자동으로 업데이트됩니다.
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+## 2) 빌드
 
-## Learn More
+아래의 명령어를 입력하여 프로젝트를 빌드할 수 있습니다.
 
-To learn more about Next.js, take a look at the following resources:
+```bash
+bun build
+```
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+## 3) 배포
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+Github의 `Main` 브랜치에 `Merge`하면 `Vercel`을 통해 자동으로 배포됩니다.
 
-## Deploy on Vercel
+여러분들은 권한이 없기 때문에 `Main` 브랜치에 `Push` 및 배포를 직접 할 수 없습니다.
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+또한 `Main` 브랜치에 직접 `Push`하지 않고 `Github Pull Request`를 통해 `Merge`를 요청해주세요.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+
+<!-- 배포를 위해서는 아래와 같은 명령어를 입력하여 빌드를 진행해주세요.
+
+```bash
+bun run build
+``` -->
--- src/utils/getTests.ts
+++ src/utils/getTests.ts
@@ -1,6 +1,15 @@
+// getTests.ts - EBS 시험지 다운로드 링크를 가져오는 함수
+
 import axios from "axios";
 import { load } from "cheerio";
 
+/**
+ * EBSi의 과목 매핑
+ * @property {Subject} key 과목명
+ * @property {number} value 과목 코드
+ * @example
+ * const subject = subjectMap["국어"]; // 1
+ */
 export const subjectMap = {
   "국어": 1,
   "수학": 2,
@@ -13,6 +22,11 @@ export const subjectMap = {
 };
 export type Subject = keyof typeof subjectMap;
 
+/**
+ * EBSi의 과목 이미지 URL 매핑
+ * @property {string} key 이미지 URL
+ * @property {Subject} value 과목명
+ */
 export const subjectUrlMap: {
   [key: string]: Subject;
 } = {
@@ -26,6 +40,15 @@ export const subjectUrlMap: {
   "/ebs/public_hsp/images/temp/thumb_book_langeage.png": "제2외국어",
 };
 
+/**
+ * 시험지 검색 조건
+ * @property {number} grade 학년
+ * @property {number[]} monthList 검색할 시험지 월
+ * @property {Subject[]} subjList 검색할 과목 리스트
+ * @property {number} beginYear 검색할 시작 년도
+ * @property {number} endYear 검색할 끝 년도
+ * @property {number} [page] 페이지(EBSi 검색 결과, 기본값: 1)
+ */
 export type Test = {
   grade: number;
   monthList: number[];
@@ -35,6 +58,14 @@ export type Test = {
   page?: number;
 };
 
+/**
+ * 시험지 정보
+ * @property {string} title 시험지 제목
+ * @property {Subject} subject 시험지 과목
+ * @property {number} questions 시험지 문항 수
+ * @property {Array<{label: string, url: string}>} buttons PDF 다운로드 버튼 리스트
+ * @property {{year: number, month: number, day: number}} date 시험 시행일
+ */
 export type TestResponse = {
   title: string;
   subject: Subject;
@@ -50,6 +81,18 @@ export type TestResponse = {
   };
 };
 
+/**
+ * EBSi 시험지 다운로드 링크를 가져오는 함수
+ * @param {Test} test 시험지 검색 조건
+ * @param {number} test.grade 학년
+ * @param {number[]} test.monthList 검색할 시험지 월
+ * @param {Subject[]} test.subjList 검색할 과목 리스트
+ * @param {number} test.beginYear 검색할 시작 년도
+ * @param {number} test.endYear 검색할 끝 년도
+ * @param {number} [test.page] 페이지(EBSi 검색 결과, 기본값: 1)
+ * @returns {Promise<TestResponse[]>} 시험지 정보 리스트
+ * @example
+ */
 export const getTests = async ({
   grade,
   monthList,
@@ -58,6 +101,7 @@ export const getTests = async ({
   endYear,
   page = 1,
 }: Test) => {
+  // EBSi 기출문제 페이지를 POST로 요청
   const response = await axios.post(
     "https://www.ebsi.co.kr/ebs/xip/xipc/previousPaperListAjax.ajax",
     new URLSearchParams([
@@ -76,15 +120,23 @@ export const getTests = async ({
   const json: TestResponse[] = [];
   for(let i = 1; i <= rowsLength; i++) {
     const rows = $(`#pagingForm > div.board_qusesion > ul > li:nth-child(${i})`)[0];
+    
+    // 이미지 URL을 통해 과목을 매핑
     const imgSrc = $(rows).find("img").attr("src");
     const thisSubject = subjectUrlMap[imgSrc || ""];
+
+    // 시험 일자를 JSON으로 변환
     const testDate = $($(rows).find(".btn_L_col2")[1]).attr("onclick")?.split("'")[5].slice(0, 8);
     const dateJson: TestResponse["date"] = {
       year: Number(testDate?.slice(0, 4) || 0),
       month: Number(testDate?.slice(4, 6) || 0),
       day: Number(testDate?.slice(6, 8) || 0),
     };
+
+    // 시험지 제목
     const textInfo =$(rows).find(".txt_info > li").map((_, info) => $(info).text().replace(/\s+/g, " ").trim().replace(/\D/g, "")).get();
+
+    // 시험지 정보를 JSON으로 변환
     const buttons = $(rows).find(".btn_L_col2").map((_, button) => {
       const label = $(button).text().replace(/\s+/g, " ").trim();
       const link = $(button).attr("onclick")?.split("'")[1];
